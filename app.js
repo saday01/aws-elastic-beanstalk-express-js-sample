2c80e15eea04800d9e52828dbab152d2379bb3ef
Saday Tiwari <saday.tiwari@gmail.com>
2021-06-10 14:51:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World This is a Test Push!'));
+app.get('/', (req, res) => res.send('Full Pipeline Test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
